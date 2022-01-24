37c60ed2aea2ff7df94b743dfacc1d088aa0bccd
Numan Qiaser <qaisernuman74@gmail.com>
2022-01-24 15:42:22 +0500

    hello

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app= express();
 
 const port = process.env.PORT || 5000;
-
+app.get("/check",(req,res)=> {
+    res.send("This is working check route");
+})
 app.get("/test",(req,res)=> {
     res.send("This is working");
 })
